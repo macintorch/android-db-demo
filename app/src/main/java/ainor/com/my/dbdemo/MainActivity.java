075281f26d9b5a3a15753eb3936b1f8dce150427
Ainor Syahrizal <macintorch@gmail.com>
2017-01-10 11:09:05 +0800

    create db, add data and make log

--- app/src/main/java/ainor/com/my/dbdemo/MainActivity.java
+++ app/src/main/java/ainor/com/my/dbdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package ainor.com.my.dbdemo;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            //create database
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users",MODE_PRIVATE, null);
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Ainor', 34)");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Asad', 5)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+
+            while (c != null) {
+                Log.i("name", c.getString(nameIndex));
+                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+
+                c.moveToNext();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
